7801a06ef8d229b8c435074534c781988193173c
Dálcio Garcia <dalciomacuetegarcia@gmail.com>
2021-07-04 18:26:21 +0100

    feat: global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { StyledThemeProvider } from '@definitions/styled-components';
+import GlobalStyles from '@styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <StyledThemeProvider>
+            <GlobalStyles />
             <Component {...pageProps} />
         </StyledThemeProvider>
     );
