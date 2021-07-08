21bb29ec498adad5bf951069fac71699ff7025f5
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2021-07-08 11:47:38 +0100

    Update add-managers.tsx

--- src/components/registers/add-managers.tsx
+++ src/components/registers/add-managers.tsx
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { TabPanel } from '@components/tabs';
 import { meaning } from '@word-book';
+import { useSnackbar } from 'react-simple-snackbar';
+import { GenericInput, Password, Phone } from '@components/inputs';
+import { Button } from '@components/button';
+import { useAuthContext } from '@context';
+import { Container } from '@components/styled-system';
+import { database, auth } from '@services';
+import { emailBuilder } from '@components/utils';
 
 export const AddManagers: React.FC = () => {
+    const [password, setPassword] = useState<string>('');
+    const [phone, setPhone] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [openSnackbar] = useSnackbar();
+    const { isLoading, startLoading, stopLoading } = useAuthContext();
+
+    const onError = (err: string) => {
+        stopLoading();
+        openSnackbar(err);
+    };
+
+    const addManagersSubmit = () => {
+        try {
+            const email = emailBuilder(phone);
+            const data: {
+                password: string;
+                avatar: string;
+                name: string;
+                email: string;
+                phone: string;
+            } = {
+                name,
+                avatar: '',
+                email,
+                phone,
+                password,
+            };
+
+            auth()
+                .createUserWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                    var user = userCredential.user;
+                    if (user) {
+                        database()
+                            .ref(`users/managers/${user.uid}`)
+                            .set(data)
+                            .then(() => {
+                                openSnackbar('Gestor cadastrado com sucesso');
+                                stopLoading();
+                            });
+                    }
+                })
+                .catch((error) => {
+                    onError('Aconteceu alguma coisa tente denovo');
+                });
+        } catch (err) {
+            stopLoading();
+        }
+    };
+
+    const onSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        startLoading();
+        addManagersSubmit();
+    };
+
     return (
-        <TabPanel label={meaning('pages.register.managers')}>
-            Add Manager
+        <TabPanel label={meaning('pages.register.add-managers')}>
+            <Container
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                textAlign="center"
+                marginBottom="16px"
+            >
+                <form onSubmit={onSubmit}>
+                    <h3>{meaning('pages.register.add-managers')}</h3>
+                    <div id="anything">
+                        Pereencha os dados do novo gestor de bombas.
+                    </div>
+                    <GenericInput
+                        value={name}
+                        changeValue={setName}
+                        label="Nome"
+                    />
+                    <Phone value={phone} changeValue={setPhone} />
+                    <Password value={password} changeValue={setPassword} />
+                    <br />
+                    <Button variant="success" mt={'16px'} isLoading={isLoading}>
+                        {meaning('pages.register.add-managers', {
+                            capitalized: true,
+                        })}
+                    </Button>
+                </form>
+            </Container>
         </TabPanel>
     );
 };
